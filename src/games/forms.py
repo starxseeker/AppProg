7e3e98292caf9da7dc7ea0bb89141cb1c5094b0b
Juho <juho.martti@edu.turkuamk.fi>
2021-12-02 16:07:40 +0200

    forms pages

---
+++ src/games/forms.py
@@ -0,0 +1,19 @@
+from django import forms
+from django.db.models import fields
+from django.forms.models import ModelMultipleChoiceField
+
+from .models import BoardGame, Genre, Review
+
+class BoardGameForm(forms.ModelForm):
+    genre = forms.ModelMultipleChoiceField(queryset=Genre.objects.all())
+
+    class Meta:
+        model = BoardGame
+        fields = ["name", "genre"]
+        labels = {"name": "Board game's name", "genre": "Board game's genre"}
+
+class ReviewForm(forms.ModelForm):
+    class Meta:
+        model = Review
+        fields = ["review", "stars"]
+        labels = {"review": "Review", "stars": "Stars"}
--- src/games/models.py
+++ src/games/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.core.validators import MaxValueValidator, MinValueValidator
 
 from django.db import models
 from django.contrib.auth.models import User
@@ -13,7 +14,10 @@ class BoardGame(models.Model):
     owner = models.ForeignKey(User, on_delete=models.CASCADE)
     name = models.CharField(max_length=200)
     genre = models.ManyToManyField(Genre, related_name="genres")
-    total_borrow_count = 0
+    total_borrow_count = models.IntegerField(default=0)
+    borrowed = models.BooleanField(default=False)
+    borrowed_by = models.ForeignKey(User, on_delete=models.CASCADE,
+    related_name="borrowed_by")
     date_borrowed = models.DateTimeField(auto_now_add = True)
     date_added = models.DateTimeField(auto_now_add = True)
     date_modified = models.DateTimeField(auto_now_add = True)
@@ -22,11 +26,13 @@ class BoardGame(models.Model):
         return f'{self.name}, {self.total_borrow_count}'
 
 class Review(models.Model):
-    board_game = models.ForeignKey(BoardGame, on_delete=models.CASCADE)
-    owner = models.ForeignKey(User, on_delete=models.CASCADE)
-    review = models.TextField()
-    date_added = models.DateTimeField(auto_now_add = True)
-    date_modified = models.DateTimeField(auto_now_add = True)
+    board_game = models.ForeignKey(BoardGame, on_delete=models.CASCADE, null=True)
+    owner = models.ForeignKey(User, on_delete=models.CASCADE, null=True)
+    review = models.TextField(null=True)
+    stars = models.IntegerField(default = 3,
+    validators=[MinValueValidator(0), MaxValueValidator(5)])
+    date_added = models.DateTimeField(auto_now_add = True, null=True)
+    date_modified = models.DateTimeField(auto_now_add = True, null=True)
 
     def __str__(self):
         return f'{self.board_game}, {self.review[:50]}'
