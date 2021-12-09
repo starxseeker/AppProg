25649d2debfc9397f267b77d30217a3009fff6f0
Leevi <leeviftw@hotmail.com>
2021-12-09 18:14:22 +0200

    forms.py changes from juho's branch

--- src/games/forms.py
+++ src/games/forms.py
@@ -14,4 +14,14 @@ class ReviewForm(forms.ModelForm):
     class Meta:
         model = Review
         fields = ["review", "stars"]
-        labels = {"review": "Review", "stars": "Stars(0-5)"}
+        labels = {"review": "Review", "stars": "Stars"}
+
+SORT_CHOICES= [
+    ('name', 'name'),
+    ('genre', 'genre'),
+    ('-total_borrow_count', 'borrow count'),
+    ('borrowed', 'availability'),
+    ]
+
+class SortForm(forms.Form):
+    sort = forms.CharField(label='Sort by', widget=forms.Select(choices=SORT_CHOICES), initial="name")
